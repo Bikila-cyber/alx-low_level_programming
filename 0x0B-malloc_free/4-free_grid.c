55c9ab7e4b65e4feff9eb546987e388c4309fd01
Bikila boja <bekyboja99@gmail.com.com>
2021-09-22 06:03:29 -0700

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously
- * created by the alloc_grid function
- * @grid: double pointer to multi-dimen array
- * @height: height of grid
- */
+  * free_grid - ...
+  * @grid: ...
+  * @height: ...
+  *
+  * Return: ...
+  */
 void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
+	{
 		free(grid[i]);
+	}
+
 	free(grid);
 }
