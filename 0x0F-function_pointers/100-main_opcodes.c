5d5c12b21d40f44f9c4b07c702e40f6fe40c3fb3
Bikila boja <bekyboja99@gmail.com.com>
2021-09-30 00:51:17 -0700

    4. Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,32 +1,30 @@
-#include "function_pointers.h"
-
-
-
-/*
-*Intresting note: functions are a set of instruction and
-*function pointers are pointers to the begning of this instruction
-*so if we could get the first instrucions adress inmemory
-*we can see what it is doing by printing the adresses
-*/
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * main - print opcodes of its own main function
- * @argc: number of arguments
- * @argv: vector of arguments
- * Return: 0 if no errors
+ * main - prints n number of opcodes from main()
+ * @argc: count of inputs
+ * @argv: array of input including n # of bytes to print
+ * Return: 0 success, 1 error, 2 error
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	register int i, num;
-	char *ptr = (char *)main;
+	int i, bytes;
 
 	if (argc != 2)
-		printf("Error\n"), exit(1);
-	num = atoi(argv[1]);
-	if (num < 0)
-		printf("Error\n"), exit(2);
-	for (i = 0; i < num - 1; i++)
-		printf("%02hhx ", ptr[i]);
-	printf("%02hhx\n", ptr[i]);
+	{
+		printf("Error\n");
+		return (1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02x", ((unsigned char *)main)[i]);
+		printf("%c", i < bytes - 1 ? ' ' : '\n');
+	}
 	return (0);
 }
