d87fd75186247cda4e5526f47d37f74c9961b63c
Bikila-cyber <bekyboja99@gmail.com>
2021-10-19 23:14:33 -0700

    this is the second one

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,39 +1,20 @@
 #include "main.h"
-/**
- * powX-powers a number b to the p's power
- * @b: base
- * @p : power
- * Return: return b to the power of a
- */
-unsigned long int powX(int b, int p)
-{
-	unsigned long int ans = 1;
-
-	while (p)
-	{
-		ans *= b;
-		p--;
-	}
-	return (ans);
-}
 
 /**
- * get_bit - returns the value at an index
- * @n: input unsigned long
- * @index: index to return
- * Return: value at input index
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit.
+ * @index: The index to get the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int test;
-
-	if (index > sizeof(n) * BIT_SIZE - 1)
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-
-	test = powX(2, index);
-	if (test & n)
-		return (1);
-	else
+	if ((n & (1 << index)) == 0)
 		return (0);
+
+	return (1);
 }
